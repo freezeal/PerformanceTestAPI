1d3fada60b68712fd3522fb6b6b1c6bf43be8856
eloy-cho <eloy.cho@kakaocorp.com>
2024-05-03 08:00:35 +0900

    계산하기 API에 사칙연산으로 분리

--- venv/app.py
+++ venv/app.py
@@ -7,13 +7,25 @@ app=Flask(__name__)
 def home():
     return render_template('HelloWorld.html')
 
-@app.route('/request_api', methods=['POST'])
-def request_api():
+@app.route('/request_api/<operation>', methods=['POST'])
+def request_api(operation):
     data = request.get_json()
     num1 = data.get('num1')
     num2 = data.get('num2')
 
-    result = num1 + num2
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return jsonify({'error': 'Cannot divide by zero'}), 400
+    else:
+        return jsonify({'error': 'Invalid operation'}), 400
 
     return jsonify({'result': result})
 
